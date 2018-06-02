f0d7a133f7313b93dc22950aa55759bbe3144809
lapwat <q.lapointe@gmail.com>
2018-06-03 01:00:50 +0200

    readme

--- iota/main.py
+++ iota/main.py
@@ -1,4 +1,5 @@
 import random
+from iota import Address
 from iota.crypto.addresses import AddressGenerator
 
 r = random.SystemRandom()
@@ -6,4 +7,4 @@ trytes = '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 seed = ''.join([r.choice(trytes) for _ in range(81)])
 address = AddressGenerator(seed).get_addresses(0)[0]
 print('Seed:         ', seed)
-print('First address:', address)
+print('First address:', Address(address).with_valid_checksum())
