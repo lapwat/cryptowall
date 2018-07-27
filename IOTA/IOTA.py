0bc56832d86b089fc708a4c152f1a72a726da39c
Quentin <q.lapointe@gmail.com>
2018-07-27 19:01:49 +0200

    removed deterministic wallet for iota

--- IOTA/IOTA.py
+++ IOTA/IOTA.py
@@ -4,19 +4,11 @@ from iota import Address
 from iota.crypto.types import Seed
 from iota.crypto.addresses import AddressGenerator
 
-if len(sys.argv) > 1:
-    print('Using', sys.argv[1], 'as seed')
-    seed = sys.argv[1]
-
-    if len(seed) < 81:
-    	print('Warning: Provided seed length is less than 81. Extending it with 9s...')
-    	seed += '9'*(81-len(seed))
-else:
-    print('Generating random seed')
-    r = random.SystemRandom()
-    trytes = '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    seed = ''.join([r.choice(trytes) for _ in range(81)])
+print('Generating random seed')
+r = random.SystemRandom()
+trytes = '9ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+seed = ''.join([r.choice(trytes) for _ in range(81)])
 
 address = AddressGenerator(seed).get_addresses(0)[0]
-print('Seed          :         ', Seed(seed))
+print('Seed          :', Seed(seed))
 print('First address :', Address(address).with_valid_checksum())
